fd39167981cd1e4ff7dafe171acd688d8ff6bae7
Chqrety <fikriaverus23@gmail.com>
2023-11-15 10:01:16 +0700

    Update v.4.2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,58 +34,40 @@ class DatabaseSeeder extends Seeder
         ]);
         $adminUser->assignRole($roles->where('name', 'admin')->first());
 
-        User::create([
-            'name' => 'siswa1',
-            'email' => 'siswa1@siswa.com',
-            'password' => bcrypt('123'),
-        ]);
-
-        User::create([
-            'name' => 'siswa2',
-            'email' => 'siswa2@siswa.com',
-            'password' => bcrypt('123'),
-        ]);
-
-        User::create([
-            'name' => 'siswa3',
-            'email' => 'siswa3@siswa.com',
-            'password' => bcrypt('123'),
-        ]);
-        $siswaUsers = User::whereIn('id', [2, 3, 4])->get();
+        for ($i = 1; $i <= 30; $i++) {
+            User::create([
+                'name' => 'siswa' . $i,
+                'email' => 'siswa' . $i . '@gmail.com',
+                'password' => bcrypt('123'),
+            ]);
+        }
+        $siswaUsers = User::whereIn('id', range(2, 31))->get();
 
         foreach ($siswaUsers as $user) {
             $user->assignRole('siswa');
         }
 
-        User::create([
-            'name' => 'guru1',
-            'email' => 'guru1@guru.com',
-            'password' => bcrypt('123'),
-        ]);
-
-        User::create([
-            'name' => 'guru2',
-            'email' => 'guru2@guru.com',
-            'password' => bcrypt('123'),
-        ]);
-        $guruUsers = User::whereIn('id', [5, 6])->get();
+        for ($i = 1; $i <= 10; $i++) {
+            User::create([
+                'name' => 'guru' . $i,
+                'email' => 'guru' . $i . '@gmail.com',
+                'password' => bcrypt('123'),
+            ]);
+        }
+        $guruUsers = User::whereIn('id', range(32, 41))->get();
 
         foreach ($guruUsers as $user) {
             $user->assignRole('guru');
         }
 
-        User::create([
-            'name' => 'perusahaan1',
-            'email' => 'perusahaan1@perusahaan.com',
-            'password' => bcrypt('123'),
-        ]);
-
-        User::create([
-            'name' => 'perusahaan2',
-            'email' => 'perusahaan2@perusahaan.com',
-            'password' => bcrypt('123'),
-        ]);
-        $perusahaanUsers = User::whereIn('id', [7, 8])->get();
+        for ($i = 1; $i <= 10; $i++) {
+            User::create([
+                'name' => 'perusahaan' . $i,
+                'email' => 'perusahaan' . $i . '@gmail.com',
+                'password' => bcrypt('123'),
+            ]);
+        }
+        $perusahaanUsers = User::whereIn('id', range(42, 51))->get();
 
         foreach ($perusahaanUsers as $user) {
             $user->assignRole('perusahaan');
